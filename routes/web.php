4a70e8b2a0863032a7848e50a520d9f2eda3bf67
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-17 11:46:14 +0100

    ADD rutas del registro

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,5 @@ Route::get('/', function () {
     return view('principal');
 });
 
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
