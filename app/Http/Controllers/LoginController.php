82b112b9f86d2df9d018022d79e02c96adde9b09
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-17 17:06:33 +0100

    ADD login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view("auth.login");
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+            return back()->with('mensaje', 'No se ha podido iniciar sesión');
+        }
+
+        return redirect()->route('posts.index')->with('success', '');
+    }
+}
