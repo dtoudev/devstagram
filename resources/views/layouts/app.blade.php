f882d3b094513bd837be93c66ee1a6b24c2947b5
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-17 10:01:04 +0100

    ADD layouts y pagina principal

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Devstagram - @yield('titulo')</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-100">
+    <header class="p-5 border-b bg-white shadow">
+        <div class="container mx-auto flex justify-between items-center">
+            <h1 class="text-3xl font-black"> DevStagram</h1>
+
+            <nav class="flex gap-2">
+                <a class="font-bold uppercase text-gray-600 items-center" href="/">Login</a>
+                <a class="font-bold uppercase text-gray-600 items-center" href="/nosotros">Crear cuenta</a>
+            </nav>
+        </div>
+
+    </header>
+
+
+    <main class="container mx-auto mt-10">
+        <h2 class="font-black text-center text-3xl mb-10">@yield('titulo')</h2>
+
+        @yield('contenido')
+
+    </main>
+
+    <footer class="text-center p-5 font-bold uppercase">
+        DevStagram - Todos los derechos reservados {{ now()->year }}
+    </footer>
+</body>
+
+</html>
