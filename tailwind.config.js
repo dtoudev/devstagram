b47abbb94f8e418bdee54258f25b43b46fba6f63
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-17 10:00:23 +0100

    ADD tailwind al proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
