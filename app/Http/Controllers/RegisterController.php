99b2b80c540892edaf99acb5ebf2d85bd14529ef
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-17 17:08:47 +0100

    MODIFY checks en el registro del usuario

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,23 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
     //
-    public function index () {
+    public function index()
+    {
         return view('auth.register');
     }
 
-    public function store(Request $request){
-       // dd(request()->all());
+    public function store(Request $request)
+    {
+        // dd(request()->all());
+
+        $request->request->add(['username' => Str::slug($request->username)]);
 
         $this->validate($request, [
-            'name'=> 'required|min:5|max:30',
-            'username'=>'required|unique:table:users|min:3|max:20',
-            'email'=> 'required|unique:users|email|max:60',
-            'password'=> 'required|min:5|max:30',
+            'name' => 'required|min:5|max:30',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required|min:5|max:30|confirmed',
         ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        auth()->attempt(['email' => $request->email, 'password' => $request->password]);
+
+        return redirect()->route('posts.index')->with('success', '');
     }
 }
