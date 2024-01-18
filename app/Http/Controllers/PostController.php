52a2b5678f7e2df1869c9112b707184d696fe411
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-18 17:41:58 +0100

    ADD funcion store index y create de los posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -12,8 +13,24 @@ class PostController extends Controller
         $this->middleware("auth");
     }
 
-    public function index()
+    public function index(User $user)
     {
-        return view("dashboard");
+        return view("dashboard", [
+            "user" => $user
+        ]);
+    }
+
+    public function create()
+    {
+        return view("posts.create");
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            "titulo" => "required|max:255",
+            'descripcion' => "required|max:255",
+            "imagen" => "required",
+        ]);
     }
 }
