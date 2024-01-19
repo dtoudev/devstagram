0c47d4ee3cad0e693a212c42492d8e083d4d2304
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-19 10:24:38 +0100

    ADD relaccion entre post y user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "titulo",
+        "descripcion",
+        "imagen",
+        "user_id",
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->select(['name', 'username']);
+    }
 }
