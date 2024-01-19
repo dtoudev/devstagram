ee82109e894fd3dea993e871802deb9f8910dbf3
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-19 10:24:53 +0100

    ADD guardar imagen en BBDD

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -32,5 +33,21 @@ class PostController extends Controller
             'descripcion' => "required|max:255",
             "imagen" => "required",
         ]);
+
+        // Post::create([
+        //     "titulo" => $request->titulo,
+        //     "descripcion" => $request->descripcion,
+        //     "imagen" => $request->imagen,
+        //     "user_id" => auth()->user()->id,
+        // ]);
+
+        $request->user()->posts()->create([
+            "titulo" => $request->titulo,
+            "descripcion" => $request->descripcion,
+            "imagen" => $request->imagen,
+            "user_id" => auth()->user()->id,
+        ]);
+
+        return redirect()->route("posts.index", auth()->user()->username)->with("success", "");
     }
 }
