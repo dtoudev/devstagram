ebe1e1fa0dab9f301e83147809fef081289bf7c1
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-22 10:10:04 +0100

    ADD vista publicacion

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('titulo')
+    {{ $post->titulo }}
+@endsection
+
+@section('contenido')
+    <div class="container mx-auto flex">
+        <div class="md:w-1/2">
+            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="{{ $post->titulo }}">
+
+            <div class="p-3">
+                0 Likes
+            </div>
+
+            <div class="p-3">
+                <p class="font-bold">{{ $post->user->username }}</p>
+                <p class="text-sm text-gray-500">{{ $post->created_at->diffForHumans() }}</p>
+                <p class="mt-5">{{ $post->descripcion }}</p>
+            </div>
+        </div>
+        <div class="md:w-1/2"></div>
+    </div>
+@endsection
