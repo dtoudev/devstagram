80d0b87e718fba1bb868c32b1be816ca151d960e
Diego Touriño <diegotourinogarcia@gmail.com>
2024-01-22 09:33:20 +0100

    ADD paginacion al muro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,4 +31,26 @@
             </div>
         </div>
     </div>
+
+    <section class="container mx-auto mt-10">
+        <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+
+        @if ($posts->count() > 0)
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                @foreach ($posts as $post)
+                    <div>
+                        <a href="">
+                            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="{{ $post->titulo }}">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+
+            <div class="my-10">
+                {{ $posts->links('pagination::tailwind') }}
+            </div>
+        @else
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay publicaciones</p>
+        @endif
+    </section>
 @endsection
